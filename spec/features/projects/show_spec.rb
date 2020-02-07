dde738e089e9f5256de61a1e48860b4a49b3f8b2
DeMarcus Kirby <tkd024@gmail.com>
2020-02-07 10:43:24 -0700

    Update test to look for average experience on show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'As a visitor to project show page', type: :feature do
     @project = @challenge.projects.create!(name: "Litfit", material: "Lamp Shade")
     @project2 = @challenge.projects.create!(name: "Fitlit", material: "Shade Lamps")
     @contestant = @project.contestants.create!(name: "Tron Kat", age: 30, hometown: "Dallas", years_of_experience: 2)
-    @contestant2 = @project.contestants.create!(name: "Kat Tron", age: 29, hometown: "Denver", years_of_experience: 1)
+    @contestant2 = @project.contestants.create!(name: "Kat Tron", age: 29, hometown: "Denver", years_of_experience: 4)
   end
 
   it 'should show individual project details' do
@@ -17,6 +17,7 @@ RSpec.describe 'As a visitor to project show page', type: :feature do
     expect(page).to have_content("Project Material: #{@project.material}")
     expect(page).to have_content("Theme of Chellenge: #{@project.challenge.theme}")
     expect(page).to have_content("Number of Contestants: 2")
+    expect(page).to have_content("Average Contestant Experience: 3")
 
     expect(page).to_not have_content(@project2.name)
     expect(page).to_not have_content(@project2.material)
