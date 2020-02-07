c2e51999f7d5da779a58f8d957d7b90ddb9e9db1
DeMarcus Kirby <tkd024@gmail.com>
2020-02-07 10:22:31 -0700

    Update test to check for number of contestants

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -2,18 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor to project show page', type: :feature do
 
+  before(:each) do
+    @challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000000)
+    @project = @challenge.projects.create!(name: "Litfit", material: "Lamp Shade")
+    @project2 = @challenge.projects.create!(name: "Fitlit", material: "Shade Lamps")
+    @contestant = @project.contestants.create!(name: "Tron Kat", age: 30, hometown: "Dallas", years_of_experience: 2)
+    @contestant2 = @project.contestants.create!(name: "Kat Tron", age: 29, hometown: "Denver", years_of_experience: 1)
+  end
+
   it 'should show individual project details' do
-    challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000000)
-    project = challenge.projects.create!(name: "Litfit", material: "Lamp Shade")
-    project2 = challenge.projects.create!(name: "Fitlit", material: "Shade Lamps")
+    visit "/projects/#{@project.id}"
 
-    visit "/projects/#{project.id}"
+    expect(page).to have_content("Project Name: #{@project.name}")
+    expect(page).to have_content("Project Material: #{@project.material}")
+    expect(page).to have_content("Theme of Chellenge: #{@project.challenge.theme}")
+    expect(page).to have_content("Number of Contestants: 2")
 
-    expect(page).to have_content("Project Name: #{project.name}")
-    expect(page).to have_content("Project Material: #{project.material}")
-    expect(page).to have_content("Theme of Chellenge: #{project.challenge.theme}")
-
-    expect(page).to_not have_content(project2.name)
-    expect(page).to_not have_content(project2.material)
+    expect(page).to_not have_content(@project2.name)
+    expect(page).to_not have_content(@project2.material)
   end
 end
